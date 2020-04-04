ab2cd9ab1c6f4e5e083d6212309f4e3042bdb771
Cole Frishman <awesomeeloc@gmail.com>
2020-04-03 21:41:33 -0400

    used var instead of let

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]);
-    let hcText = "!HC";
+    var hcText = "!HC";
 
   if(request.text && request.text.contains(hcText)) {
     this.res.writeHead(200);
